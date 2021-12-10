fa00798d781010bafcfa73a0d797cfa736e861a3
amal <amal.141726@gmail.com>
2021-12-10 16:38:02 +0300

    finish the MabVC

--- MyApp/Controller/MapVC.swift
+++ MyApp/Controller/MapVC.swift
@@ -9,56 +9,44 @@ import UIKit
 import MapKit
 class MapVC: UIViewController {
     
-    let mapView : MKMapView = {
-        let map = MKMapView()
-        map.overrideUserInterfaceStyle = .light
-        return map
-    }()
+        let mapView : MKMapView = {
+            let map = MKMapView()
+            map.overrideUserInterfaceStyle = .light
+            map.translatesAutoresizingMaskIntoConstraints = false
+            return map
+        }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setMapConstraints()
-    }
-    func setMapConstraints() {
-        view.addSubview(mapView)
-        mapView.translatesAutoresizingMaskIntoConstraints = false
-        mapView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        mapView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-        mapView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        mapView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+    
+                view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
         
-        let Aseer = MKPointAnnotation()
-        Aseer.title = "Aseer central hospital"
-        Aseer.coordinate = CLLocationCoordinate2D(latitude: 18.19953, longitude: 42.52503)
-        mapView.addAnnotation(Aseer)
+        createAnnotations(locations: annotationLocations)
+    }
+    
+    let annotationLocations = [
+        ["title": "King faisal medical city", "latitude": 18.19648, "longitude": 42.70139],
+        ["title": "Aseer central hospital", "latitude": 18.19925, "longitude":42.52484],
+        ["title": "Uhud Rafidah General Hospital", "latitude": 18.17310, "longitude":42.84903],
+        ["title": "Mustasharak Hospital", "latitude": 18.28490, "longitude":42.73103],
+        ["title": "German Hospital", "latitude": 18.2813590, "longitude":42.66912],
+        ["title": "Saudi Specialist Medical Center", "latitude": 18.28130, "longitude":42.66913],
+        ["title": "Khamis Mushait General Hospital", "latitude": 18.27476, "longitude":42.73476]
+         ]
 
-        let medicalCity = MKPointAnnotation()
-        medicalCity.title = "King faisal medical city"
-        medicalCity.coordinate = CLLocationCoordinate2D(latitude: 24.7135517, longitude: 46.6752957)
-        mapView.addAnnotation(medicalCity)
-        
-        let generalHospital = MKPointAnnotation()
-        generalHospital.title = "Uhud Rafidah General Hospital"
-        generalHospital.coordinate = CLLocationCoordinate2D(latitude: 18.084764, longitude: 43.138569)
-        mapView.addAnnotation(generalHospital)
-        
-        let mustasharak = MKPointAnnotation()
-        mustasharak.title = "Mustasharak Hospital"
-        mustasharak.coordinate = CLLocationCoordinate2D(latitude: 18.084764, longitude: 43.138569)
-        mapView.addAnnotation(mustasharak)
-        
-        let german = MKPointAnnotation()
-        german.title = "German Hospital"
-        german.coordinate = CLLocationCoordinate2D(latitude: 18.084764, longitude: 43.138569)
-        mapView.addAnnotation(german)
-        
-        let medicalCenter = MKPointAnnotation()
-        medicalCenter.title = "Saudi Specialist Medical Center"
-        medicalCenter.coordinate = CLLocationCoordinate2D(latitude: 18.084764, longitude: 43.138569)
-        mapView.addAnnotation(medicalCenter)
-        
-        let gH = MKPointAnnotation()
-        gH.title = "Khamis Mushait General Hospital"
-        gH.coordinate = CLLocationCoordinate2D(latitude: 18.084764, longitude: 43.138569)
-        mapView.addAnnotation(gH)
+    func createAnnotations(locations: [[String : Any]]) {
+        for location in locations {
+             let annotations = MKPointAnnotation()
+             annotations.title = location["title"] as? String
+             annotations.coordinate = CLLocationCoordinate2D(latitude: location["latitude"] as! CLLocationDegrees,
+            longitude: location["longitude"] as! CLLocationDegrees)
+            mapView.addAnnotation(annotations)
+        }
     }
-}
+         }
